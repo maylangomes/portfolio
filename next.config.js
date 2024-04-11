b7b4cc5e1011748602ac1f71a48e6e69e8c138f2
Maylan <maylan.gomes@epitech.eu>
2024-04-11 11:57:53 +0200

    fix: next config rename and change content to basic config

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  basePath: "/Portfolio",
-  output: "export",
-  reactStrictMode: true,
-  swcMinify: true,
-}
-
-module.exports = nextConfig
